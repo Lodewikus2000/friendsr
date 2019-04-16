4e8eb047f7ea6358509d8537fdc73a7d7b23d674
Lodewikus2000 <32392003+Lodewikus2000@users.noreply.github.com>
2019-04-16 17:50:30 +0200

    werkt niet

---
+++ app/src/main/java/com/example/friendsr/FriendsAdapter.java
@@ -0,0 +1,50 @@
+package com.example.friendsr;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendsAdapter extends ArrayAdapter<Friend> {
+
+    private int resource;
+    private ArrayList<Friend> friends;
+
+    // constructor
+    public FriendsAdapter(Context context, int resource, ArrayList<Friend> objects) {
+        super(context, resource, objects);
+
+        this.friends = objects;
+        this.resource = resource;
+
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
+        }
+
+        ImageView image = convertView.findViewById(R.id.profilePic);
+        TextView text = convertView.findViewById(R.id.profileText);
+
+        Friend currentFriend = friends.get(position);
+
+        
+
+        image.setImageResource(currentFriend.getDrawableId());
+        text.setText(currentFriend.getName());
+
+
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/friendsr/MainActivity.java
+++ app/src/main/java/com/example/friendsr/MainActivity.java
@@ -2,12 +2,69 @@ package com.example.friendsr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Friend> friends = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            int aryaInt = this.getResources().getIdentifier("@drawable/arya", "drawable", "android.demo");
+            Friend arya = new Friend("Arya Stark", "She is no one.", aryaInt);
+            friends.add(arya);
+
+            int cerseiInt = this.getResources().getIdentifier("cersei", "drawable", "android.demo");
+            Friend cersei = new Friend("Cersei Lannister", "Protector of the realm.", cerseiInt);
+            friends.add(cersei);
+
+            int daenerysInt = this.getResources().getIdentifier("daenerys", "drawable", "android.demo");
+            Friend daenerys = new Friend("Daenerys Targaryen", "Mother of dragons.", daenerysInt);
+            friends.add(daenerys);
+
+            int jaimeInt = this.getResources().getIdentifier("jaime", "drawable", "android.demo");
+            Friend jaime = new Friend("Jaime Lannister", "Kingslayer.", jaimeInt);
+            friends.add(jaime);
+
+            int jonInt = this.getResources().getIdentifier("jon", "drawable", "android.demo");
+            Friend jon = new Friend("Jon Snow", "Knows nothing.", jonInt);
+            friends.add(jon);
+
+            int jorahInt = this.getResources().getIdentifier("jorah", "drawable", "android.demo");
+            Friend jorah = new Friend("Jorah of Mormont", "Greyscale survivor.", jorahInt);
+            friends.add(jorah);
+
+            int margaeryInt = this.getResources().getIdentifier("margaery", "drawable", "android.demo");
+            Friend margaery = new Friend("Margaery Tyrell", "Died.", margaeryInt);
+            friends.add(margaery);
+
+            int melisandreInt = this.getResources().getIdentifier("melisandre", "drawable", "android.demo");
+            Friend melisandre = new Friend("Melisandre", "Annoying.", melisandreInt);
+            friends.add(melisandre);
+
+            int sansaInt = this.getResources().getIdentifier("sansa", "drawable", "android.demo");
+            Friend sansa = new Friend("Sansa Stark", "Used to be married to Tyrion.", sansaInt);
+            friends.add(sansa);
+
+            int tyrionInt = this.getResources().getIdentifier("tyrion", "drawable", "android.demo");
+            Friend tyrion = new Friend("Tyrion Lannister", "Drinks and knows things.", tyrionInt);
+            friends.add(tyrion);
+        }
+
+
+        FriendsAdapter adapter = new FriendsAdapter(this, R.layout.grid_item, friends);
+
+        GridView gridje = findViewById(R.id.mainGrid);
+        gridje.setAdapter(adapter);
+
+
     }
 }
